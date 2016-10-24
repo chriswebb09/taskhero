17689b655c4e8c9beb4fc870c6d481c5724e1bd5
chriswebb09 <christopher.webb@flatironschool.com>
2016-10-24 10:10:30 -0400

    work on settings view controller and cell

--- TaskHero/TaskHero/Controllers/SettingsViewController.swift
+++ TaskHero/TaskHero/Controllers/SettingsViewController.swift
@@ -9,20 +9,35 @@
 import UIKit
 
 class SettingsViewController: UITableViewController {
+    var settings = [String]()
+    ///let settings = ["User Settings", "Profile", "Application Settings"]
     
-    let settings = ["User Settings", "Profile", "Application Settings"]
+    let userSettings = ["Username", "Email", "First Name", "Last Name", "Tasks Completed", "Friends"]
+    
+    let applicationSettings = ["Notifications", "Log Out", "Stay Logged In"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let segmentControl = UISegmentedControl(items: ["User Settings", "Application Settings"])
         edgesForExtendedLayout = []
+        view.addSubview(segmentControl)
+        segmentControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentControl.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height * 0.01).isActive = true
+        segmentControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        segmentControl.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier:0.96).isActive = true
+        segmentControl.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier:Constants.loginFieldHeight).isActive = true
+        segmentControl.addTarget(self, action: #selector(changeView), for: .valueChanged)
+        
         tableView.register(SettingsCell.self, forCellReuseIdentifier: SettingsCell.cellIdentifier)
         tableView.tableFooterView = UIView(frame: CGRect.zero)
         tableView.separatorStyle = .singleLine
-        tableView.allowsSelection = false
+        //tableView.allowsSelection = false
         tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = view.frame.height / 4
+        tableView.estimatedRowHeight = 40
+        //tableView.estimatedRowHeight = view.frame.height / 2
         tableView.layoutMargins = UIEdgeInsets.zero
         tableView.separatorInset = UIEdgeInsets.zero
+        settings = userSettings
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,15 +50,46 @@ class SettingsViewController: UITableViewController {
         return 1
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return settings.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let settingsCell = tableView.dequeueReusableCell(withIdentifier: SettingsCell.cellIdentifier, for: indexPath as IndexPath) as! SettingsCell
         settingsCell.layoutSubviews()
-        settingsCell.taskNameLabel.text = settings[indexPath.row]
+        settingsCell.taskDescriptionLabel.text = settings[indexPath.row]
         return settingsCell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     //   if settings[indexPath.row] ==
+//        print(settings[indexPath.row])
+//        let alertController = UIAlertController(title: "Invalid", message: "\(settings[indexPath.row])", preferredStyle: UIAlertControllerStyle.alert)
+//        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+//            print("OK")
+//        }
+//        alertController.addAction(okAction)
+//        self.present(alertController, animated: true, completion: nil)
+       // return
+    }
+    
+    func changeView(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("Segment Changed to Said By")
+            settings = userSettings
+        default:
+            print("Segment Changed to Heard By")
+            settings = applicationSettings
+        }
+        tableView.reloadData()
+    }
+
     
 }
--- TaskHero/TaskHero/Views/Cells/SettingsCell.swift
+++ TaskHero/TaskHero/Views/Cells/SettingsCell.swift
@@ -12,48 +12,45 @@ class SettingsCell: UITableViewCell {
     
     static let cellIdentifier = "SettingsCell"
     
-    let taskNameLabel: UITextView = {
-        let textView = UITextView()
-        textView.textColor = UIColor.black
-        textView.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
-        textView.textAlignment = .left
-        textView.layer.masksToBounds = true
-        textView.isScrollEnabled = false
-        textView.isEditable = false
-        textView.isUserInteractionEnabled = false
-        return textView
-    }()
+//    let taskNameLabel: UITextView = {
+//        let textView = UITextView()
+//        textView.textColor = UIColor.black
+//        textView.font = UIFont(name: "HelveticaNeue-Thin", size: 22)
+//        textView.textAlignment = .left
+//        textView.layer.masksToBounds = true
+//        textView.isScrollEnabled = false
+//        textView.isEditable = false
+//        return textView
+//    }()
     
     let taskDescriptionLabel: UITextView = {
         let textView = UITextView()
         textView.backgroundColor = UIColor(red:0.01, green:0.47, blue:0.74, alpha:1.0)
         textView.textColor = UIColor.white
         textView.layer.cornerRadius = 2
-        textView.font = UIFont(name: "HelveticaNeue-Thin", size: 18)
-        textView.textAlignment = .left
+        textView.font = UIFont(name: Constants.helveticaLight, size: 18)
+        textView.textAlignment = .center
         textView.layer.masksToBounds = true
         textView.isEditable = false
-        textView.isUserInteractionEnabled = false
         textView.isScrollEnabled = false
         return textView
     }()
     
-    let taskDueLabel: UITextView = {
-        let textView = UITextView()
-        textView.textColor = UIColor.black
-        textView.font = UIFont(name:"HelveticaNeue-Thin", size: 16)
-        textView.textAlignment = .left
-        textView.layer.masksToBounds = true
-        textView.isScrollEnabled = false
-        textView.isEditable = false
-        textView.isUserInteractionEnabled = false
-        return textView
-    }()
+//    let taskDueLabel: UITextView = {
+//        let textView = UITextView()
+//        textView.textColor = UIColor.black
+//        textView.font = UIFont(name:"HelveticaNeue-Thin", size: 16)
+//        textView.textAlignment = .left
+//        textView.layer.masksToBounds = true
+//        textView.isScrollEnabled = false
+//        textView.isUserInteractionEnabled = false
+//        return textView
+//    }()
     
-    lazy var taskCompletedView: UIImageView = {
-        let taskCompletedImageView = UIImageView()
-        return taskCompletedImageView
-    }()
+//    lazy var taskCompletedView: UIImageView = {
+//        let taskCompletedImageView = UIImageView()
+//        return taskCompletedImageView
+//    }()
     
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -63,42 +60,43 @@ class SettingsCell: UITableViewCell {
     
     func setupConstraints() {
         
-        contentView.addSubview(taskNameLabel)
+//        contentView.addSubview(taskNameLabel)
+//        
+//        taskNameLabel.translatesAutoresizingMaskIntoConstraints = false
+//        taskNameLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.2).isActive = true
+//        taskNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.65).isActive = true
+//        taskNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20).isActive = true
+//        taskNameLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant:10).isActive = true
         
-        taskNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        taskNameLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.2).isActive = true
-        taskNameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.65).isActive = true
-        taskNameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20).isActive = true
-        taskNameLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant:10).isActive = true
+      //  contentView.addSubview(taskDueLabel)
         
-        contentView.addSubview(taskDueLabel)
-        
-        taskDueLabel.translatesAutoresizingMaskIntoConstraints = false
-        taskDueLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.2).isActive = true
-        taskDueLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.65).isActive = true
-        taskDueLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
-        taskDueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 60).isActive = true
+//        taskDueLabel.translatesAutoresizingMaskIntoConstraints = false
+//        taskDueLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.2).isActive = true
+//        taskDueLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.65).isActive = true
+//        taskDueLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10).isActive = true
+//        taskDueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 60).isActive = true
         
         contentView.addSubview(taskDescriptionLabel)
         
         taskDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        taskDescriptionLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.38).isActive = true
-        taskDescriptionLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.94).isActive = true
+        taskDescriptionLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.35).isActive = true
+        taskDescriptionLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1).isActive = true
         taskDescriptionLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        taskDescriptionLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 110).isActive = true
+        taskDescriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        //taskDescriptionLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        //taskDescriptionLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 40).isActive = true
         
-        contentView.addSubview(taskCompletedView)
-        
-        taskCompletedView.translatesAutoresizingMaskIntoConstraints = false
-        taskCompletedView.heightAnchor.constraint(equalToConstant: 120).isActive = true
-        taskCompletedView.widthAnchor.constraint(equalToConstant: 120).isActive = true
-        taskCompletedView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20).isActive = true
-        taskCompletedView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+//        contentView.addSubview(taskCompletedView)
+//        
+//        taskCompletedView.translatesAutoresizingMaskIntoConstraints = false
+//        taskCompletedView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+//        taskCompletedView.widthAnchor.constraint(equalToConstant: 120).isActive = true
+//        taskCompletedView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20).isActive = true
+//        taskCompletedView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        taskNameLabel.text = ""
         taskDescriptionLabel.text = ""
     }
 }
