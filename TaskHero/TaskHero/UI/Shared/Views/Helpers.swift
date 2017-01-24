ede43c597078307d7a03f48b3afd91e1a236207c
chriswebb09 <christopher.webb@flatironschool.com>
2017-01-24 13:42:11 -0500

    removed unecessary files

--- TaskHero/TaskHero/UI/Shared/Views/Helpers.swift
+++ TaskHero/TaskHero/UI/Shared/Views/Helpers.swift
@@ -13,18 +13,6 @@ import Firebase
 
 public extension UITableView {
     
-//    fileprivate lazy var dateFormatter1: DateFormatter = {
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "yyyy/MM/dd"
-//        return formatter
-//    }()
-//    fileprivate lazy var dateFormatter2: DateFormatter = {
-//        let formatter = DateFormatter()
-//        formatter.dateFormat = "yyyy-MM-dd"
-//        return formatter
-//    }()
-
-    
     public func setupTableView() {
         estimatedRowHeight = Constants.Settings.rowHeight
         layoutMargins = UIEdgeInsets.zero
