90b9d2357114e67276d79b362a9d3b2a061de92a
chriswebb09 <christopher.webb@flatironschool.com>
2017-01-28 12:28:43 -0500

    added app screen viewmodel

---
+++ TaskHero/TaskHero/UI/AppScreen/AppScreenViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  AppScreenViewModel.swift
+//  TaskHero
+//
+//  Created by Christopher Webb-Orenstein on 1/28/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+
+class AppScreenViewModel {
+    
+    var signupButtonColor: UIColor = {
+        return Constants.Color.backgroundColor
+    }()
+    
+    var signupButtonText: String = {
+        return "Register Now"
+    }()
+    
+    var viewDividerBackgroundColor: UIColor = {
+        return UIColor.lightGray
+    }()
+    
+    var logoImage: UIImage = {
+        return UIImage()
+    }()
+
+}
