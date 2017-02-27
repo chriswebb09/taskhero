8ad622364bc9e7784136b88493250a1bd52e227a
chriswebb09 <christopher.webb@flatironschool.com>
2017-02-27 14:24:15 -0500

    update to constants

--- TaskHero/TaskHero/UI/AddTask/AddTaskView.swift
+++ TaskHero/TaskHero/UI/AddTask/AddTaskView.swift
@@ -73,21 +73,21 @@ final class AddTaskView: UIView {
     
     fileprivate func setupConstraints() {
         configureView(view: taskNameLabel)
-        taskNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: bounds.height * 0.05).isActive = true
+        taskNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: bounds.height * AddTaskViewConstants.nameLabelTopMultiplier).isActive = true
         configureView(view: taskNameField)
-        taskNameField.topAnchor.constraint(equalTo: taskNameLabel.bottomAnchor, constant: bounds.height * 0.05).isActive = true
+        taskNameField.topAnchor.constraint(equalTo: taskNameLabel.bottomAnchor, constant: bounds.height * AddTaskViewConstants.nameLabelTopMultiplier).isActive = true
         addSubview(taskDescriptionBox)
         taskDescriptionBox.translatesAutoresizingMaskIntoConstraints = false
         taskDescriptionBox.widthAnchor.constraint(equalTo: widthAnchor, multiplier: AddTaskViewConstants.widthMultiplier).isActive = true
-        taskDescriptionBox.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.3).isActive = true
+        taskDescriptionBox.heightAnchor.constraint(equalTo: heightAnchor, multiplier: AddTaskViewConstants.descriptionBoxHeightMultiplier).isActive = true
         taskDescriptionBox.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         taskDescriptionBox.topAnchor.constraint(equalTo: taskNameField.bottomAnchor, constant: bounds.height * Constants.Dimension.settingsOffset).isActive = true
         addSubview(addTaskButton)
         addTaskButton.translatesAutoresizingMaskIntoConstraints = false
-        addTaskButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.4).isActive = true
+        addTaskButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: AddTaskViewConstants.addTaskButtonWidthMultiplier).isActive = true
         addTaskButton.heightAnchor.constraint(equalTo: heightAnchor, multiplier: AddTaskViewConstants.heightMultiplier).isActive = true
         addTaskButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        addTaskButton.topAnchor.constraint(equalTo: taskDescriptionBox.bottomAnchor, constant: bounds.height *  0.05).isActive = true
+        addTaskButton.topAnchor.constraint(equalTo: taskDescriptionBox.bottomAnchor, constant: bounds.height *  AddTaskViewConstants.addTaskButtonTopOffset).isActive = true
     }
     
     func animatedPosition() {
@@ -100,9 +100,8 @@ final class AddTaskView: UIView {
             taskDescriptionBox.layer.borderColor = UIColor.gray.cgColor
         }
         
-        taskNameField.topAnchor.constraint(equalTo: topAnchor, constant: self.bounds.height * 0.04)
-        
-        taskDescriptionBox.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.2)
+        taskNameField.topAnchor.constraint(equalTo: topAnchor, constant: self.bounds.height * AddTaskViewConstants.taskNameFieldTopMultiplier)
+        taskDescriptionBox.heightAnchor.constraint(equalTo: heightAnchor, multiplier: AddTaskViewConstants.taskDescriptionBoxHeightMultiplier)
         taskDescriptionBox.topAnchor.constraint(equalTo: taskNameField.bottomAnchor, constant: self.bounds.height * AddTaskViewConstants.topOffset)
         addTaskButton.topAnchor.constraint(equalTo: taskDescriptionBox.bottomAnchor, constant: self.bounds.height * AddTaskViewConstants.topOffset)
         
@@ -116,5 +115,10 @@ struct AddTaskViewConstants {
     static let topOffset: CGFloat = 0.02
     static let widthMultiplier: CGFloat = 0.85
     static let heightMultiplier: CGFloat = 0.07
-    
+    static let nameLabelTopMultiplier: CGFloat = 0.05
+    static let descriptionBoxHeightMultiplier:CGFloat = 0.3
+    static let addTaskButtonWidthMultiplier: CGFloat = 0.4
+    static let addTaskButtonTopOffset: CGFloat = 0.05
+    static let taskNameFieldTopMultiplier: CGFloat = 0.04
+    static let taskDescriptionBoxHeightMultiplier: CGFloat = 0.2
 }
